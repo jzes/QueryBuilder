ed63534f3fbd226fe35f90a01774075ab5a78d63
Jose De Paulo Da Silva Neto <jpsneto@paschoalotto.com.br>
2019-04-27 12:23:40 -0300

    start of a new feature and end the PKFeature

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.Common;
+using Npgsql;
 
 namespace tests
 {
@@ -7,22 +9,34 @@ namespace tests
         static void Main(string[] args)
         {
             var johnDoe = new People();
-            johnDoe.Id = 156;
-            johnDoe.FirstName = "John";
-            johnDoe.LastName = "John";
-            johnDoe.Document = "123456";
-            johnDoe.Age = 28;
-            johnDoe.BirdDate = DateTime.Now;
-            johnDoe.Value = 2300.50;
+            // johnDoe.Id = 156;
+            // johnDoe.FirstName = "John";
+            // johnDoe.LastName = "John";
+            // johnDoe.Contrato = "123456";
+            // johnDoe.Age = 28;
+            // johnDoe.BirdDate = DateTime.Now;
+            // johnDoe.Value = 2300.50;
 
-            System.Console.WriteLine(johnDoe.BuildInsert());
-            System.Console.WriteLine("\n---\n");
-            System.Console.WriteLine(johnDoe.BuildUpdate());
-            System.Console.WriteLine("\n---\n");
-            System.Console.WriteLine(johnDoe.BuildSelect()+johnDoe.BuildWhereByKey());
-            System.Console.WriteLine("\n---\n");
-            System.Console.WriteLine(johnDoe.BuildDelete());
+            // System.Console.WriteLine(johnDoe.BuildInsert());
+            // System.Console.WriteLine("\n---\n");
+            // System.Console.WriteLine(johnDoe.BuildUpdate());
+            // System.Console.WriteLine("\n---\n");
+            // System.Console.WriteLine(johnDoe.BuildSelect()+johnDoe.BuildWhereByKey());
+            // System.Console.WriteLine("\n---\n");
+            // System.Console.WriteLine(johnDoe.BuildDelete());
             
+            var con = new NpgsqlConnection("");
+            
+            using(con){
+                con.Open();
+                var comm = new NpgsqlCommand("select * from pessoas limit 10", con);
+                DbDataReader data = comm.ExecuteReader();
+                while(data.Read()){
+                    johnDoe.GetUm(data);
+                    System.Console.WriteLine(johnDoe.Contrato);
+                    System.Console.WriteLine(johnDoe.FirstName);
+                }
+            }
         }
     }
 }
